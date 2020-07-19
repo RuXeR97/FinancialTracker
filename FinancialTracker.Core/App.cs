270b239a21e9e9a81bb00c893f8b95511ef17be0
Rafał Dębski <ruxer0097@gmail.com>
2020-07-19 23:55:24 +0200

    Start of repository

---
+++ FinancialTracker.Core/App.cs
@@ -0,0 +1,17 @@
+﻿using FinancialTracker.Core.Services;
+using FinancialTracker.Core.ViewModels;
+using MvvmCross;
+using MvvmCross.ViewModels;
+
+namespace FinancialTracker.Core
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            Mvx.IoCProvider.RegisterType<ICalculationService, CalculationService>();
+
+            RegisterAppStart<TipViewModel>();
+        }
+    }
+}
---
+++ FinancialTracker.Core/Services/CalculationService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinancialTracker.Core.Services
+{
+    public class CalculationService : ICalculationService
+    {
+        public double TipAmount(double subTotal, int generosity)
+        {
+            return subTotal * ((double)generosity) / 100.0;
+        }
+    }
+
+    public interface ICalculationService
+    {
+        double TipAmount(double subTotal, int generosity);
+    }
+}
